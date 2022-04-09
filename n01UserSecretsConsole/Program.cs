752684e14e52d224d4b132e468c5e3e65ef323b9
Alex Chow <zhouchuanglin@gmail.com>
2022-04-09 02:19:11 +0200

    n01UserSecrets

---
+++ n01UserSecretsConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.Extensions.Configuration;  
+IConfiguration? Config;
+
+Config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+var apiKey = Config["ApiKey"];
+Console.WriteLine(apiKey);
