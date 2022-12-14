5ebb5d8d83b86a0fcc208c23154a3aab14e676dd
Alex chow <zhouchuanglin@gmail.com>
2022-12-14 04:33:37 +0100

    Update VideoPlayerOption.cs

--- b13video/Pages/VideoPlayerOption.cs
+++ b13video/Pages/VideoPlayerOption.cs
@@ -73,4 +73,29 @@ namespace b13video.Pages
         public string Src { get; set; } = "application/x-mpegURL";
     }
 
+//     <source src = "rtmp://rtc.qiscus.com:2935/live360p/testing123&testing.mp4" type="rtmp/mp4">
+//  </source>
+
+//    rtmp/mp4
+//11 :   opus: 'video/ogg',
+//12 :   ogv: 'video/ogg',
+//13 :   mp4: 'video/mp4',
+//14 :   mov: 'video/mp4',
+//15 :   m4v: 'video/mp4',
+//16 :   mkv: 'video/x-matroska',
+//17 :   m4a: 'audio/mp4',
+//18 :   mp3: 'audio/mpeg',
+//19 :   aac: 'audio/aac',
+//20 :   caf: 'audio/x-caf',
+//21 :   flac: 'audio/flac',
+//22 :   oga: 'audio/ogg',
+//23 :   wav: 'audio/wav',
+//24 :   m3u8: 'application/x-mpegURL',
+//25 :   mpd: 'application/dash+xml',
+//26 :   jpg: 'image/jpeg',
+//27 :   jpeg: 'image/jpeg',
+//28 :   gif: 'image/gif',
+//29 :   png: 'image/png',
+//30 :   svg: 'image/svg+xml',
+//31 :   webp: 'image/webp'
 }
