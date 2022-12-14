4b6768f465a234d584b274219972f884280c1eb6
Alex chow <zhouchuanglin@gmail.com>
2022-12-14 01:04:32 +0100

    5. m3u8

---
+++ b13video/wwwroot/app.js
@@ -0,0 +1,15 @@
+﻿function loadPlayer(id, options) {
+    var player = videojs(id, options);
+
+    player.ready(function () {
+        var promise = player.play();
+
+        if (promise !== undefined) {
+            promise.then(function () {
+                console.log('Autoplay started!');
+            }).catch(function (error) {
+                console.log('Autoplay was prevented.',error);
+            });
+        }
+    });
+}
