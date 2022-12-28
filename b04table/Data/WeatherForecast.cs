c8b6d08c286a6ac90ce820bf674201ccccf5e7a4
Alex Chow <zhouchuanglin@gmail.com>
2022-12-28 11:46:01 +0100

    添加 b04table with  BootstrapBlazor 7.1.8

---
+++ b04table/Data/WeatherForecast.cs
@@ -0,0 +1,26 @@
+﻿using BootstrapBlazor.Components;
+using FreeSql.DataAnnotations;
+using Magicodes.ExporterAndImporter.Excel;
+using OfficeOpenXml.Table;
+using System.ComponentModel;
+
+namespace b14table.Data;
+
+[ExcelImporter(IsLabelingError = true)]
+[ExcelExporter(Name = "导入商品中间表", TableStyle = TableStyles.Light10, AutoFitAllColumn = true)]
+[AutoGenerateClass(Searchable = true, Filterable = true, Sortable = true)]
+public class WeatherForecast
+{
+    [Column(IsIdentity = true)]
+    [DisplayName("序号")]
+    public int ID { get; set; }
+
+    [DisplayName("日期")]
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
