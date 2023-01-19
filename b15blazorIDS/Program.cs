ef2bdd8519c2c08a04dd5640497fd1d364c4d1d5
Alex Chow <zhouchuanglin@gmail.com>
2023-01-19 13:36:29 +0100

    b15blazorIDS - 改为 Sqlite

--- b15blazorIDS/Program.cs
+++ b15blazorIDS/Program.cs
@@ -1,4 +1,4 @@
-// ********************************** 
+﻿// ********************************** 
 // Densen Informatica 中讯科技 
 // 作者：Alex Chow
 // e-mail:zhouchuanglin@gmail.com 
@@ -15,13 +15,30 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//EF SqlServer 配置
+
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+//EF Sqlite 配置
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseSqlite(builder.Configuration.GetConnectionString("IdsSQliteConnection")));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+builder.Services.AddDefaultIdentity<IdentityUser>(o =>
+ {   // Password settings.
+     o.Password.RequireDigit = false;
+     o.Password.RequireLowercase = false;
+     o.Password.RequireNonAlphanumeric = false;
+     o.Password.RequireUppercase = false;
+     o.Password.RequiredLength = 4;
+     o.Password.RequiredUniqueChars = 1;
+ }
+)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
