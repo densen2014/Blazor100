b301b697968b2e86f8f3f5b18e8f7976f65e5e27
Alex Chow <zhouchuanglin@gmail.com>
2023-01-19 11:36:19 +0100

    添加工程 b15blazorIDS

---
+++ b15blazorIDS/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace b15blazorIDS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
