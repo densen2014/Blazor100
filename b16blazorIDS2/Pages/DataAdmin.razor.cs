2b9e03893e476b784d70c582466746602aab751d
Alex chow <zhouchuanglin@gmail.com>
2023-01-20 02:16:59 +0100

    ids初始化测试账号和Role

--- b16blazorIDS2/Pages/DataAdmin.razor.cs
+++ b16blazorIDS2/Pages/DataAdmin.razor.cs
@@ -28,10 +28,14 @@ namespace b16blazorIDS2.Pages
         [NotNull]
         ToastService? ToastService { get; set; }
 
+        WebAppIdentityUser objUser = new WebAppIdentityUser();
+        string CurrentUserRole { get; set; } = AuthorizeRoles.User.ToString();
+        string strError = "";
+
 
         public bool IsExcel { get; set; }
         public bool DoubleClickToEdit { get; set; } = true;
- 
+
         private Task IsExcelToggle()
         {
             IsExcel = !IsExcel;
@@ -65,29 +69,245 @@ namespace b16blazorIDS2.Pages
                 }
             }
 
-            var chekRole = RoleManager.RoleExistsAsync(AuthorizeRoles.Admin.ToString());
+            await AddNewRole(AuthorizeRoles.Admin);
+            await AddNewRole(AuthorizeRoles.Superuser);
+            await AddNewRole(AuthorizeRoles.User);
+            await AddNewRole(AuthorizeRoles.R110);
+            await AddNewRole(AuthorizeRoles.R120);
+            await AddNewRole(AuthorizeRoles.R130);
+            await AddNewRole(AuthorizeRoles.R140);
+            
+            await AddUser("admin",AuthorizeRoles.Admin);
+            await AddUser("super",AuthorizeRoles.Superuser);
+            await AddUser("u01",AuthorizeRoles.User);
+            await AddUser("u02",AuthorizeRoles.User);
+            await AddUser("u03",AuthorizeRoles.User);
+            await AddUser("u04",AuthorizeRoles.User);
+            await AddUser("r01",AuthorizeRoles.R110);
+            await AddUser("r02",AuthorizeRoles.R120);
+
+        }
+
+        private async Task<Task<bool>> AddNewRole(AuthorizeRoles AuthorizeRole)
+        {
+            var chekRole = RoleManager.RoleExistsAsync(AuthorizeRole.ToString());
             if (chekRole.Result == false)
             {
-                await RoleManager.CreateAsync(new IdentityRole(AuthorizeRoles.Admin.ToString()));
-                await ToastService.Information("Admin Role Created");
+                await RoleManager.CreateAsync(new IdentityRole(AuthorizeRole.ToString()));
+                await ToastService.Information($"{AuthorizeRole} Role Created");
+
             }
 
-            chekRole = RoleManager.RoleExistsAsync(AuthorizeRoles.Superuser.ToString());
-            if (chekRole.Result == false)
+            return chekRole;
+        }
+
+        async Task AddUser(string UserName, AuthorizeRoles UserRole, WebAppIdentityUser? newUser = null) => await AddUser(UserName, UserRole.ToString(), newUser);
+        async Task AddUser(string UserName,string UserRole, WebAppIdentityUser? newUser = null)
+        {
+            var user = await UserManager.FindByNameAsync(UserName);
+            if (user != null) return;
+
+            var NewUser = newUser ??
+                new WebAppIdentityUser
+                {
+                    UserName = UserName,
+                    Email = $"{UserName}@app.com",
+                    PhoneNumber = UserName,
+                    UUID = Guid.NewGuid().ToString(),
+                    EmailConfirmed = true,
+                    UserRole = UserRole,
+                };
+            await ToastService.Information($"{UserName}@app.com Created");
+
+            var CreateResult =
+                await UserManager
+                .CreateAsync(NewUser, "0");
+
+            if (!CreateResult.Succeeded)
             {
-                await RoleManager.CreateAsync(new IdentityRole(AuthorizeRoles.Superuser.ToString()));
-                await ToastService.Information("Superuser Role Created");
+                if (CreateResult
+                    .Errors
+                    .FirstOrDefault() != null)
+                {
+                    strError =
+                        CreateResult
+                        .Errors
+                        .FirstOrDefault()?
+                        .Description ?? "";
+                }
+                else
+                {
+                    strError = "Create error";
+                }
+                await ToastService.Error(strError);
 
             }
-
-            chekRole = RoleManager.RoleExistsAsync(AuthorizeRoles.R110.ToString());
-            if (chekRole.Result == false)
+            else
             {
-                await RoleManager.CreateAsync(new IdentityRole(AuthorizeRoles.R110.ToString()));
-                await ToastService.Information("R110 Role Created");
-
+                await UserManager.AddToRoleAsync(NewUser, CurrentUserRole);
+                await ToastService.Information($"{UserName}@app.com Add To Role {CurrentUserRole}");
             }
+        }
 
+        async Task SaveUser(bool RefreshUsers)
+        {
+            try
+            {
+                #region 已存在的用户
+                if (objUser.Id != "")
+                {
+                    // 获取用户
+                    var user = await UserManager.FindByIdAsync(objUser.Id);
+                    if (user == null) return;
+
+                    // 更新电子邮件
+                    user.UserName = objUser.UserName;
+                    user.Email = objUser.Email;
+                    user.UUID = objUser.UUID;
+                    user.PhoneNumber = objUser.PhoneNumber;
+                    user.EmailConfirmed = objUser.EmailConfirmed;
+                    user.UserRole = CurrentUserRole;
+
+                    // 更新用户
+                    await UserManager.UpdateAsync(user);
+
+                    // 只有当前值才更新密码
+                    // 不是默认值
+                    if (objUser.PasswordHash != "*****")
+                    {
+                        var resetToken =
+                            await UserManager.GeneratePasswordResetTokenAsync(user);
+
+                        var passworduser =
+                            await UserManager.ResetPasswordAsync(
+                                user,
+                                resetToken,
+                                objUser.PasswordHash ?? "");
+
+                        if (!passworduser.Succeeded)
+                        {
+                            if (passworduser.Errors.FirstOrDefault() != null)
+                            {
+                                strError =
+                                    passworduser
+                                    .Errors
+                                    .FirstOrDefault()?
+                                    .Description ?? "";
+                            }
+                            else
+                            {
+                                strError = "Pasword error";
+                            }
+
+                            // 保持弹出窗口打开
+                            return;
+                        }
+                    }
+
+                    // 处理角色
+
+
+                    var UserResult = await UserManager.GetRolesAsync(user);
+
+                    if (UserResult.FirstOrDefault() != null)
+                    {
+                        //删掉组
+                        foreach (var item in UserResult)
+                        {
+                            await UserManager.RemoveFromRoleAsync(user, item);
+
+                        }
+                    }
+                    await UserManager.AddToRoleAsync(user, CurrentUserRole);
+
+
+                    #endregion
+
+                }
+                else
+                {
+                    #region 新用户Insert new user
+
+                    var NewUser =
+                    new WebAppIdentityUser
+                    {
+                        UserName = objUser.UserName,
+                        Email = objUser.Email,
+                        PhoneNumber = objUser.PhoneNumber,
+                        UUID = objUser.UUID,
+                        EmailConfirmed = objUser.EmailConfirmed,
+                        UserRole = CurrentUserRole,
+                    };
+
+                    var CreateResult =
+                        await UserManager
+                        .CreateAsync(NewUser, objUser.PasswordHash ?? "");
+
+                    if (!CreateResult.Succeeded)
+                    {
+                        if (CreateResult
+                            .Errors
+                            .FirstOrDefault() != null)
+                        {
+                            strError =
+                                CreateResult
+                                .Errors
+                                .FirstOrDefault()?
+                                .Description ?? "";
+                        }
+                        else
+                        {
+                            strError = "Create error";
+                        }
+
+                        await ToastService.Error($"{objUser.Email} Save error {strError}");
+                        // 保持弹出窗口打开
+                        return;
+                    }
+                    else
+                    {
+                        await UserManager.AddToRoleAsync(NewUser, CurrentUserRole);
+                    }
+                    #endregion
+                }
+
+
+                if (RefreshUsers)
+                {
+                    // 刷新用户
+                    //GetUsers();
+                    //GetUserRolesAsync();
+                    await ToastService.Information($"{objUser.Email} Save Role {CurrentUserRole}");
+                }
+            }
+            catch (Exception ex)
+            {
+                strError = ex.GetBaseException().Message;
+            }
+        }
+
+        async Task EditUser(WebAppIdentityUser _IdentityUser)
+        {
+            // 设置选中的用户
+            // 作为当前用户
+            objUser = _IdentityUser;
+
+            // 获取用户
+            var user = await UserManager.FindByIdAsync(objUser.Id);
+            if (user != null)
+            {
+                // 用户是管理员角色吗？
+                var UserResult = await UserManager.GetRolesAsync(user);
+
+                if (UserResult.FirstOrDefault() == null)
+                {
+                    CurrentUserRole = "用户";
+                }
+                else
+                {
+                    CurrentUserRole = UserResult.First();
+                }
+            }
 
         }
 
