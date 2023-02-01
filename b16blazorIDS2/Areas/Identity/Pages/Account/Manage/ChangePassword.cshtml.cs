493e8035f3815892da6108754f4f1e177a57a0b5
Alex Chow <zhouchuanglin@gmail.com>
2023-02-01 18:55:56 +0100

    本地化资料页面,登录栏等

--- b16blazorIDS2/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ b16blazorIDS2/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -5,124 +5,123 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
-using b16blazorIDS2.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using b16blazorIDS2.Models;
 
-namespace b16blazorIDS2.Areas.Identity.Pages.Account.Manage
+namespace b16blazorIDS2.Areas.Identity.Pages.Account.Manage;
+
+public class ChangePasswordModel : PageModel
 {
-    public class ChangePasswordModel : PageModel
+    private readonly UserManager<WebAppIdentityUser> _userManager;
+    private readonly SignInManager<WebAppIdentityUser> _signInManager;
+    private readonly ILogger<ChangePasswordModel> _logger;
+
+    public ChangePasswordModel(
+        UserManager<WebAppIdentityUser> userManager,
+        SignInManager<WebAppIdentityUser> signInManager,
+        ILogger<ChangePasswordModel> logger)
     {
-        private readonly UserManager<WebAppIdentityUser> _userManager;
-        private readonly SignInManager<WebAppIdentityUser> _signInManager;
-        private readonly ILogger<ChangePasswordModel> _logger;
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _logger = logger;
+    }
 
-        public ChangePasswordModel(
-            UserManager<WebAppIdentityUser> userManager,
-            SignInManager<WebAppIdentityUser> signInManager,
-            ILogger<ChangePasswordModel> logger)
+    /// <summary>
+    /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+    /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+    /// </summary>
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    /// <summary>
+    /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+    /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+    /// </summary>
+    [TempData]
+    public string StatusMessage { get; set; }
+
+    /// <summary>
+    /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+    /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+    /// </summary>
+    public class InputModel
+    {
+        /// <summary>
+        /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+        /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "当前密码")]
+        public string OldPassword { get; set; }
+
+        /// <summary>
+        /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+        /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+        /// </summary>
+        [Required]
+        [StringLength(100, ErrorMessage = "{0} 的长度必须至少为 {2}，最多为 {1} 个字符.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "新密码")]
+        public string NewPassword { get; set; }
+
+        /// <summary>
+        /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+        /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+        /// </summary>
+        [DataType(DataType.Password)]
+        [Display(Name = "确认新密码")]
+        [Compare("NewPassword", ErrorMessage = "新密码和确认密码不匹配.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _logger = logger;
+            return NotFound($"无法加载具有 ID 的用户 '{_userManager.GetUserId(User)}'.");
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [BindProperty]
-        public InputModel Input { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string StatusMessage { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public class InputModel
+        var hasPassword = await _userManager.HasPasswordAsync(user);
+        if (!hasPassword)
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
-            [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
-            public string OldPassword { get; set; }
-
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "New password")]
-            public string NewPassword { get; set; }
-
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
+            return RedirectToPage("./SetPassword");
         }
 
-        public async Task<IActionResult> OnGetAsync()
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
         {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            var hasPassword = await _userManager.HasPasswordAsync(user);
-            if (!hasPassword)
-            {
-                return RedirectToPage("./SetPassword");
-            }
-
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            var changePasswordResult = await _userManager.ChangePasswordAsync(user, Input.OldPassword, Input.NewPassword);
-            if (!changePasswordResult.Succeeded)
-            {
-                foreach (var error in changePasswordResult.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-                return Page();
-            }
-
-            await _signInManager.RefreshSignInAsync(user);
-            _logger.LogInformation("User changed their password successfully.");
-            StatusMessage = "Your password has been changed.";
-
-            return RedirectToPage();
+            return NotFound($"无法加载具有 ID 的用户 '{_userManager.GetUserId(User)}'.");
         }
+
+        var changePasswordResult = await _userManager.ChangePasswordAsync(user, Input.OldPassword, Input.NewPassword);
+        if (!changePasswordResult.Succeeded)
+        {
+            foreach (var error in changePasswordResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return Page();
+        }
+
+        await _signInManager.RefreshSignInAsync(user);
+        _logger.LogInformation("用户修改密码成功.");
+        StatusMessage = "您的密码已被更改.";
+
+        return RedirectToPage();
     }
 }
--- b16blazorIDS2/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ b16blazorIDS2/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -7,166 +7,166 @@ using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
-using b16blazorIDS2.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using b16blazorIDS2.Models;
 
-namespace b16blazorIDS2.Areas.Identity.Pages.Account.Manage
+namespace b16blazorIDS2.Areas.Identity.Pages.Account.Manage;
+
+public class EmailModel : PageModel
 {
-    public class EmailModel : PageModel
+    private readonly UserManager<WebAppIdentityUser> _userManager;
+    private readonly SignInManager<WebAppIdentityUser> _signInManager;
+    private readonly IEmailSender _emailSender;
+
+    public EmailModel(
+        UserManager<WebAppIdentityUser> userManager,
+        SignInManager<WebAppIdentityUser> signInManager,
+        IEmailSender emailSender)
     {
-        private readonly UserManager<WebAppIdentityUser> _userManager;
-        private readonly SignInManager<WebAppIdentityUser> _signInManager;
-        private readonly IEmailSender _emailSender;
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _emailSender = emailSender;
+    }
 
-        public EmailModel(
-            UserManager<WebAppIdentityUser> userManager,
-            SignInManager<WebAppIdentityUser> signInManager,
-            IEmailSender emailSender)
+    /// <summary>
+    /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+    /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+    /// </summary>
+    [Display(Name = "邮箱")]
+    public string Email { get; set; }
+
+    /// <summary>
+    /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+    /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+    /// </summary>
+    public bool IsEmailConfirmed { get; set; }
+
+    /// <summary>
+    /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+    /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+    /// </summary>
+    [TempData]
+    public string StatusMessage { get; set; }
+
+    /// <summary>
+    /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+    /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+    /// </summary>
+    [BindProperty]
+    public InputModel Input { get; set; }
+
+    /// <summary>
+    /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+    /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+    /// </summary>
+    public class InputModel
+    {
+        /// <summary>
+        /// 此 API 支持 ASP.NET Core Identity 默认 UI 基础结构，不打算使用
+        /// 直接来自您的代码。此 API 可能会在未来的版本中更改或删除。
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        [Display(Name = "新邮箱")]
+        public string NewEmail { get; set; }
+    }
+
+    private async Task LoadAsync(WebAppIdentityUser user)
+    {
+        var email = await _userManager.GetEmailAsync(user);
+        Email = email;
+
+        Input = new InputModel
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _emailSender = emailSender;
+            NewEmail = email,
+        };
+
+        IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
+    }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound($"无法加载具有 ID 的用户 '{_userManager.GetUserId(User)}'.");
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public string Email { get; set; }
+        await LoadAsync(user);
+        return Page();
+    }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public bool IsEmailConfirmed { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string StatusMessage { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [BindProperty]
-        public InputModel Input { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public class InputModel
+    public async Task<IActionResult> OnPostChangeEmailAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
-            [EmailAddress]
-            [Display(Name = "New email")]
-            public string NewEmail { get; set; }
+            return NotFound($"无法加载具有 ID 的用户 '{_userManager.GetUserId(User)}'.");
         }
 
-        private async Task LoadAsync(WebAppIdentityUser user)
+        if (!ModelState.IsValid)
         {
-            var email = await _userManager.GetEmailAsync(user);
-            Email = email;
-
-            Input = new InputModel
-            {
-                NewEmail = email,
-            };
-
-            IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
-        }
-
-        public async Task<IActionResult> OnGetAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
             await LoadAsync(user);
             return Page();
         }
 
-        public async Task<IActionResult> OnPostChangeEmailAsync()
+        var email = await _userManager.GetEmailAsync(user);
+        if (Input.NewEmail != email)
         {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
-                return Page();
-            }
-
-            var email = await _userManager.GetEmailAsync(user);
-            if (Input.NewEmail != email)
-            {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmailChange",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, email = Input.NewEmail, code = code },
-                    protocol: Request.Scheme);
-                await _emailSender.SendEmailAsync(
-                    Input.NewEmail,
-                    "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
-                return RedirectToPage();
-            }
-
-            StatusMessage = "Your email is unchanged.";
-            return RedirectToPage();
-        }
-
-        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
-                return Page();
-            }
-
             var userId = await _userManager.GetUserIdAsync(user);
-            var email = await _userManager.GetEmailAsync(user);
-            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
             code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
             var callbackUrl = Url.Page(
-                "/Account/ConfirmEmail",
+                "/Account/ConfirmEmailChange",
                 pageHandler: null,
-                values: new { area = "Identity", userId = userId, code = code },
+                values: new { area = "Identity", userId = userId, email = Input.NewEmail, code = code },
                 protocol: Request.Scheme);
             await _emailSender.SendEmailAsync(
-                email,
+                Input.NewEmail,
                 "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                $"请通过以下方式确认您的帐户 <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>点击这里</a>.");
 
-            StatusMessage = "Verification email sent. Please check your email.";
+            StatusMessage = "Confirmation link to change email sent. Please check your email.";
             return RedirectToPage();
         }
+
+        StatusMessage = "Your email is unchanged.";
+        return RedirectToPage();
+    }
+
+    public async Task<IActionResult> OnPostSendVerificationEmailAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound($"无法加载具有 ID 的用户 '{_userManager.GetUserId(User)}'.");
+        }
+
+        if (!ModelState.IsValid)
+        {
+            await LoadAsync(user);
+            return Page();
+        }
+
+        var userId = await _userManager.GetUserIdAsync(user);
+        var email = await _userManager.GetEmailAsync(user);
+        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+        var callbackUrl = Url.Page(
+            "/Account/ConfirmEmail",
+            pageHandler: null,
+            values: new { area = "Identity", userId = userId, code = code },
+            protocol: Request.Scheme);
+        await _emailSender.SendEmailAsync(
+            email,
+            "Confirm your email",
+            $"请通过以下方式确认您的帐户 <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>点击这里</a>.");
+
+        StatusMessage = "Verification email sent. Please check your email.";
+        return RedirectToPage();
     }
 }
