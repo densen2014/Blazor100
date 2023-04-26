80ddcf81ed4654cf93eecdc22c44ea3e040be467
Alex chow <zhouchuanglin@gmail.com>
2023-04-26 20:09:41 +0200

    Blazor UI库 Bootstrap Blazor 快速上手 (v7.5.7)
    https://www.cnblogs.com/densen2014/p/17357815.html

---
+++ b18QuickStartv757/Shared/NavMenu.razor.cs
@@ -0,0 +1,20 @@
+﻿using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace b18QuickStartv757.Shared;
+
+public partial class NavMenu
+{
+    private IEnumerable<MenuItem> Menus { get; set; } = new List<MenuItem>
+    {
+            new MenuItem() { Text = "首页", Url = "/"  , Match = NavLinkMatch.All},
+            new MenuItem() { Text = "Counter", Url = "/counter" },
+            new MenuItem() { Text = "Fetch data", Url = "/fetchdata" },
+            new MenuItem() { Text = "工具" ,Items= new List<MenuItem>
+                {
+                    new MenuItem() { Text = "Counter", Url = "/counter" },
+                    new MenuItem() { Text = "Fetch data", Url = "/fetchdata" },
+               }
+            },
+    };
+}
