1df0dc8b940c064c311a937de1dbf37253ee70c5
Alex chow <zhouchuanglin@gmail.com>
2023-12-20 01:01:01 +0100

    Blazor入门100天 : 自做一个手势滑动组件
    https://www.cnblogs.com/densen2014/p/17915288.html

---
+++ b20Gesture/Components/Pages/EnumGesture.cs
@@ -0,0 +1,34 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using System.ComponentModel;
+
+namespace b20Gesture.Components;
+
+
+public enum EnumGesture
+{
+    [Description("无手势")]
+    None,
+
+    [Description("向左滑动")]
+    Left,
+
+    [Description("向右滑动")]
+    Right,
+
+    [Description("回退手势")]
+    Back,
+
+    [Description("翻页手势")]
+    PageUp,
+
+    [Description("刷新手势")]
+    Refresh,
+
+    [Description("退出手势")]
+    Exit,
+}
---
+++ b20Gesture/Program.cs
@@ -0,0 +1,33 @@
+// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using b20Gesture.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
