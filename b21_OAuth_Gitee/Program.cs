6142c489920507679a3f2c4482921c9927097c22
Alex chow <zhouchuanglin@gmail.com>
2024-01-11 22:58:26 +0100

    Blazor Wasm Google 登录
    https://www.cnblogs.com/densen2014/p/17959857

---
+++ b21_OAuth_Gitee/Program.cs
@@ -0,0 +1,29 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using b21_OAuth_Gitee;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//google
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.Authority = "https://accounts.google.com/";
+    options.ProviderOptions.ClientId = "*******.apps.googleusercontent.com";
+    options.ProviderOptions.ResponseType = "id_token token";
+    options.ProviderOptions.RedirectUri = "https://localhost:5001/authentication/login-callback";
+    options.ProviderOptions.PostLogoutRedirectUri = "/";
+    options.UserOptions.AuthenticationType = "google";
+});
+
+await builder.Build().RunAsync();
