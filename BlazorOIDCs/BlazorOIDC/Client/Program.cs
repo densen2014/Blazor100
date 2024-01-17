653885a9da62fffd3750b76a3c82f5e18e73cf01
Alex chow <zhouchuanglin@gmail.com>
2024-01-17 22:04:41 +0100

    配置 独立WASM端授权/独立WASM端演示站点的API授权

--- BlazorOIDCs/BlazorOIDC/Client/Program.cs
+++ BlazorOIDCs/BlazorOIDC/Client/Program.cs
@@ -1,4 +1,4 @@
-// ********************************** 
+﻿// ********************************** 
 // Densen Informatica 中讯科技 
 // 作者：Alex Chow
 // e-mail:zhouchuanglin@gmail.com 
@@ -9,16 +9,85 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+var useServerAPI = false;
+
+//使用真实的演示站点的API
+var useServerAPIDemo = false;
+
+//只是WASM客户端,不使用自托管服务器端API
+var isClientOnly = false;
+
+var serverAPIclientID = !useServerAPIDemo?"BlazorOIDC.ServerAPI":"BlazorWasmIdentity.ServerAPI";
+var oidcClientID = !useServerAPIDemo? "BlazorOIDC.Client" : "BlazorWasmIdentity.Localhost";
+
+//服务器应用的 API 授权终结点 URL
+var apiEndPoint = string.Empty;
+var callBackURL = string.Empty;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("BlazorOIDC.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+apiEndPoint = !isClientOnly? builder.HostEnvironment.BaseAddress:"https://ids2.app1.es/";
+callBackURL = apiEndPoint;
 
-// Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorOIDC.ServerAPI"));
+if (useServerAPI)
+{
+    //如果要将 Blazor WebAssembly 应用配置为使用不属于托管 Blazor 解决方案的现有 Identity Server 实例，请将 HttpClient基地址注册从IWebAssemblyHostEnvironment.BaseAddress ( builder.HostEnvironment.BaseAddress) 更改为服务器应用的 API 授权终结点 URL。
 
-builder.Services.AddApiAuthorization();
+    builder.Services.AddHttpClient(serverAPIclientID, client => client.BaseAddress = new Uri(apiEndPoint))
+        .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+    // 在向服务器项目发出请求时提供包含访问令牌的 Http 客户端实例
+    builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(serverAPIclientID));
+    builder.Services.AddApiAuthorization();
+
+}
+else
+{
+
+
+ #if DEBUG
+
+    if (isClientOnly)
+    {
+        callBackURL = apiEndPoint;
+        apiEndPoint = "https://ids2.app1.es/";
+    }
+
+#else
+    apiEndPoint = "https://ids2.app1.es/";
+    callBackURL = apiEndPoint;
+    oidcClientID = "BlazorWasmIdentity.Client";
+#endif
+
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiEndPoint) });
+
+    //完整的配置
+    builder.Services.AddOidcAuthentication(options =>
+    {
+        options.ProviderOptions.Authority = apiEndPoint;
+        options.ProviderOptions.ClientId = oidcClientID;
+        options.ProviderOptions.ResponseType = "code";
+        options.ProviderOptions.RedirectUri = $"{callBackURL}authentication/login-callback";
+        options.ProviderOptions.PostLogoutRedirectUri = $"{callBackURL}authentication/logout-callback";
+        options.ProviderOptions.DefaultScopes.Add("openid");
+        options.ProviderOptions.DefaultScopes.Add("profile");
+    });
+
+    //用第三方登录,只能验证是否登录, 不能验证用户是否有权限. 调用接口时,还是需要验证用户是否有权限
+
+
+    //IDS演示站点登录 https://demo.duendesoftware.com/
+    //builder.Services.AddOidcAuthentication(options =>
+    //{
+    //    options.ProviderOptions.Authority = "https://demo.duendesoftware.com/";
+    //    options.ProviderOptions.ClientId = "interactive.confidential";
+    //    options.ProviderOptions.ResponseType = "code";
+    //    options.ProviderOptions.RedirectUri = $"{callBackURL}authentication/login-callback";
+    //    options.ProviderOptions.PostLogoutRedirectUri = $"{callBackURL}authentication/logout-callback"; 
+
+    //});
+}
 
 await builder.Build().RunAsync();
