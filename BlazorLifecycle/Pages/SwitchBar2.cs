92f769afc9c2291a4f32b75bd18aedd660af8afe
Alex chow <zhouchuanglin@gmail.com>
2024-01-17 10:22:08 +0100

    组件继承
    https:https://www.cnblogs.com/densen2014/p/17970545

---
+++ BlazorLifecycle/Pages/SwitchBar2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+
+namespace BlazorLifecycle.Pages;
+
+
+public class SwitchBar2 : SwitchBar  
+{
+    [Parameter]
+    public int Value2 { get; set; }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        ToolBar= RenderComponents();
+    }
+
+    private static RenderFragment RenderComponents() => builder =>
+    {
+        builder.OpenComponent<ToolBarBase>(0);
+        builder.CloseComponent(); 
+    };
+}
