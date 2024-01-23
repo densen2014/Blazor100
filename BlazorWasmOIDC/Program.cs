f5df4d1fba9af46d3ade01a1a7c1975d9fa494bb
Alex chow <zhouchuanglin@gmail.com>
2024-01-23 15:48:56 +0100

    独立WASM端授权
    https://www.cnblogs.com/densen2014/p/17969485

---
+++ BlazorWasmOIDC/Program.cs
@@ -0,0 +1,33 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using BlazorWasmOIDC;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+var authority = "https://localhost:5001/";
+var clientId = "Blazor5002";
+var url2 = builder.HostEnvironment.BaseAddress;
+
+//完整的配置
+builder.Services.AddOidcAuthentication(options =>
+{
+    options.ProviderOptions.Authority = authority;
+    options.ProviderOptions.ClientId = clientId;
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.RedirectUri = $"{url2}authentication/login-callback";
+    options.ProviderOptions.PostLogoutRedirectUri = $"{url2}authentication/logout-callback";
+    options.ProviderOptions.DefaultScopes.Add("BlazorWasmIdentity.ServerAPI");
+    options.UserOptions.RoleClaim = "role";
+});
+
+await builder.Build().RunAsync();
