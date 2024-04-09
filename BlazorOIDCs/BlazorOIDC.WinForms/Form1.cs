820f5f4c5a96f71f73de099bcd6f773b27a54da9
Alex chow <zhouchuanglin@gmail.com>
2024-04-10 01:37:30 +0200

    独立 BlazorOIDC.WinForms  端授权

---
+++ BlazorOIDCs/BlazorOIDC.WinForms/Form1.cs
@@ -0,0 +1,27 @@
+// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+
+namespace BlazorOIDC.WinForms;
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+
+        var blazor = new BlazorWebView()
+        {
+            Dock = DockStyle.Fill,
+            HostPage = "wwwroot/index.html",
+            Services = Startup.Services!,
+            StartPath = "/counter"
+        };
+
+        blazor.RootComponents.Add<Main>("#app");
+        Controls.Add(blazor);
+    }
+}
