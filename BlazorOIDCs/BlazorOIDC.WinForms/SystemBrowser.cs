29a01c8427814d70c8b541437001c16697e182e9
Alex chow <zhouchuanglin@gmail.com>
2024-04-10 03:50:18 +0200

    Update SystemBrowser.cs

--- BlazorOIDCs/BlazorOIDC.WinForms/SystemBrowser.cs
+++ BlazorOIDCs/BlazorOIDC.WinForms/SystemBrowser.cs
@@ -2,15 +2,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using System;
 using System.Diagnostics;
-using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 #nullable disable
 
 namespace BlazorOIDC.WinForms;
