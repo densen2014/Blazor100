1f36cbecd4cf90413b08ef0b30bd7105b08198b9
Alex chow <zhouchuanglin@gmail.com>
2024-07-05 13:57:11 +0200

    Blazor 逐键搜索并动态反馈到url -1
    https://www.cnblogs.com/densen2014/p/18286508

---
+++ b22dynamicURL/b22dynamicURL.Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using b22dynamicURL.Server.Components;
+using Microsoft.AspNetCore.SignalR;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.AddBootstrapBlazor();
+
+// 增加 SignalR 服务数据传输大小限制配置
+builder.Services.Configure<HubOptions>(option => option.MaximumReceiveMessageSize = null);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
