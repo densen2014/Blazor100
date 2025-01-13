ca3f87760596f70ea618b42c73bb96309e77ab74
Alex chow <zhouchuanglin@gmail.com>
2025-01-13 12:38:30 +0100

    b23multipleLanguages  为你的Blazor程序加入本地化多语言功能
    https://www.cnblogs.com/densen2014/p/18669229

---
+++ b23multipleLanguages/BootstrapBlazorApp.Server/Program.cs
@@ -0,0 +1,62 @@
+﻿using BootstrapBlazor.Components;
+using BootstrapBlazorApp.Server.Components;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Options;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+builder.Services.AddBootstrapBlazor();
+
+// 增加多语言支持配置信息
+builder.Services.AddRequestLocalization<IOptionsMonitor<BootstrapBlazorOptions>>((localizerOption, blazorOption) =>
+{
+    blazorOption.OnChange(Invoke);
+    Invoke(blazorOption.CurrentValue);
+    return;
+
+    void Invoke(BootstrapBlazorOptions option)
+    {
+        var supportedCultures = option.GetSupportedCultures();
+        localizerOption.SupportedCultures = supportedCultures;
+        localizerOption.SupportedUICultures = supportedCultures;
+    }
+});
+
+builder.Services.AddControllers();
+
+// 增加 Pdf 导出服务
+builder.Services.AddBootstrapBlazorTableExportService();
+
+// 增加 Html2Pdf 服务
+builder.Services.AddBootstrapBlazorHtml2PdfService();
+
+// 增加 SignalR 服务数据传输大小限制配置
+builder.Services.Configure<HubOptions>(option => option.MaximumReceiveMessageSize = null);
+
+var app = builder.Build();
+
+// 启用本地化
+var option = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+if (option != null)
+{
+    app.UseRequestLocalization(option.Value);
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapDefaultControllerRoute();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+app.Run();
