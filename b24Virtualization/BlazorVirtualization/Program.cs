5d92ff6da8b2607b5fda9f208df0cf5146396b53
Alex chow <zhouchuanglin@gmail.com>
2025-01-17 12:27:18 +0100

    Blazor 通过组件虚拟化提高性能
    https://www.cnblogs.com/densen2014/p/18677409

---
+++ b24Virtualization/BlazorVirtualization/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
