40263c006a700a82887f3df727ca4d5432e1ca7a
Alex chow <zhouchuanglin@gmail.com>
2025-06-09 13:43:51 +0200

    Update Program.cs

--- Test01NCalc/Program.cs
+++ Test01NCalc/Program.cs
@@ -31,9 +31,30 @@ EvaluateParameterHandler value = (string name, ParameterArgs args) =>
         args.Result = 3.14;
 };
 
+void Expr_EvaluateFunction(string name, FunctionArgs args)
+{
+    if (name == "Weight")
+    {
+        // 计算重量
+        if (product.BultoPrice >0)
+        {
+            args.Result = $"1 {(string.IsNullOrWhiteSpace(product.Unit)? "Litro/ 1 Kg" : product.Unit)} {product.BultoPrice} Euros";
+        } 
+    }else if (name == "VIP")
+    {
+        // 计算价格
+        args.Result = product.Name.StartsWith('*')? "<有会员折扣>" : string.Empty;
+    }
+}
+
 expr = new Expression("if([product.Name] LIKE '*%','<有会员折扣>','')", ExpressionOptions.StringConcat);
-expr.Parameters["product.Name"] = product.Name;
 expr.EvaluateParameter += value;
+res = expr.Evaluate();
+
+Console.WriteLine(res); // 输出: <有会员折扣>
+
+expr = new Expression("VIP()", ExpressionOptions.StringConcat);
+expr.EvaluateFunction += Expr_EvaluateFunction;
 res = expr.Evaluate();
 
 Console.WriteLine(res); // 输出: <有会员折扣>
@@ -45,5 +66,12 @@ Console.WriteLine(res); // 输出: <折后价>
 
 expr = new Expression("if([product.BultoPrice] > 0, '1 '  + if(([product.Unit]=='' || [product.Unit]==null),'Litro/ 1 Kg',[product.Unit]) + ' ' + [product.BultoPrice] + ' Euros', '')", ExpressionOptions.StringConcat | ExpressionOptions.AllowNullParameter);
 expr.EvaluateParameter += value;
+res = expr.Evaluate();
+Console.WriteLine(res); // 输出: <每公斤价格> 1 Litro/ 1 Kg 3.90 Euros
+
+expr = new Expression("Weight()", ExpressionOptions.StringConcat | ExpressionOptions.AllowNullParameter);
+expr.EvaluateFunction += Expr_EvaluateFunction;
+
+
 res = expr.Evaluate();
 Console.WriteLine(res); // 输出: <每公斤价格> 1 Litro/ 1 Kg 3.90 Euros
