7a55d30688c98d0d547052daffad66e7b46121fb
Alex chow <zhouchuanglin@gmail.com>
2025-06-09 13:35:12 +0200

    NCalc

---
+++ Test01NCalc/Program.cs
@@ -0,0 +1,49 @@
+﻿using NCalc;
+using NCalc.Handlers;
+using System.Reflection;
+using Test01NCalc;
+
+var product = new Product("*Coca cola", "001", 1.55m);
+product.Discount = 10m; // 设置折扣属性
+product.BultoPrice = 18.00m; // 设置批量价格属性 
+product.Unit = "Kg"; // 设置单位属性
+
+var expr = new Expression("2 + 3 * 5");
+var res= expr.Evaluate();
+
+
+EvaluateParameterHandler value = (string name, ParameterArgs args) =>
+{
+    // 只处理以 "product." 开头的参数
+    if (name.StartsWith("product.", StringComparison.OrdinalIgnoreCase))
+    {
+        var propName = name.Substring("product.".Length);
+        // 通过反射获取属性值
+        var prop = typeof(Product).GetProperty(propName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+        if (prop != null)
+        {
+            args.Result = prop.GetValue(product);
+            return;
+        }
+    }
+    // 其他参数处理
+    if (name == "Pi")
+        args.Result = 3.14;
+};
+
+expr = new Expression("if([product.Name] LIKE '*%','<有会员折扣>','')", ExpressionOptions.StringConcat);
+expr.Parameters["product.Name"] = product.Name;
+expr.EvaluateParameter += value;
+res = expr.Evaluate();
+
+Console.WriteLine(res); // 输出: <有会员折扣>
+
+expr = new Expression("if([product.Discount] > 0, [product.Price] * [product.Discount], [product.Price])", ExpressionOptions.StringConcat);
+expr.EvaluateParameter += value;
+res = expr.Evaluate();
+Console.WriteLine(res); // 输出: <折后价>
+
+expr = new Expression("if([product.BultoPrice] > 0, '1 '  + if(([product.Unit]=='' || [product.Unit]==null),'Litro/ 1 Kg',[product.Unit]) + ' ' + [product.BultoPrice] + ' Euros', '')", ExpressionOptions.StringConcat | ExpressionOptions.AllowNullParameter);
+expr.EvaluateParameter += value;
+res = expr.Evaluate();
+Console.WriteLine(res); // 输出: <每公斤价格> 1 Litro/ 1 Kg 3.90 Euros
---
+++ Test01NCalc/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+using NCalc;
+using System.Diagnostics;
+using Xunit; 
+
+namespace Test01NCalc;
+
+class Product(string name, string code, decimal price)
+{
+    public string Name { get; set; } = name;
+    public string Code { get; set; } = code;
+    public decimal Discount { get; set; }
+    public decimal Price { get; set; } = price;
+    public decimal BultoPrice { get; set; }  
+    public string? Weight { get; set; }
+    public string? Unit { get; set; }
+}
+
+public class ProductTests
+{
+    [Fact]
+    public void Product_Constructor_SetsProperties()
+    {
+        var product = new Product("*Coca cola", "001", 1.55m);
+        var expression = new Expression("2 + 3 * 5");
+        Assert.Equal(17 ,(int) expression.Evaluate());
+
+        expression = new Expression("if(product.Name LIKE '*%','','<有会员折扣>')");
+        expression.Parameters["product"] = product;
+        Assert.Equal("<有会员折扣>", expression.Evaluate());
+
+    }
+} 
